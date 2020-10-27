57a27fd5c6d71685e83fe5f0ebb43182fb544b7a
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-27 21:26:25 +0100

    Zadatak 10 update - 2. kolokvijum

--- 2-kolokvijum/OPOS -solution -2nd_midterm -zad10.java
+++ 2-kolokvijum/OPOS -solution -2nd_midterm -zad10.java
@@ -5,11 +5,15 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 
-public class SalesCountryReducer extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, Text> {
+public class SalesCountryReducer extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, Text,> {
 
-    public void reduce(IntWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) {
-        if (key.get() > 2018 && value.toString().startsWith("On")) {
-            output.collect(key,value);
-        }
+    public void reduce(IntWritable t_key, Iterator<Text> values, OutputCollector<IntWritable, Text> output, Reporter reporter) {
+		IntWritable key = t_key;
+		while(values.hasNext()) {
+			Text publication = (Text) values.next();
+			if (key.get() > 2018 && publication.toString().startsWith("On")) {
+				output.collect(t_key,value);
+			}
+		}
     }
 }
