919133523335a3b72b5ea5132dd3df78602d3f50
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-27 16:13:21 +0100

    Zadatak 10 rjesenje - 2. kolokvijum

---
+++ 2-kolokvijum/OPOS -solution -2nd_midterm -zad10.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+public class SalesCountryReducer extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, Text,> {
+
+    public void reduce(IntWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) {
+        if (key.get() > 2018 && value.toString().startsWith("On")) {
+            output.collect(key,value);
+        }
+    }
+}
