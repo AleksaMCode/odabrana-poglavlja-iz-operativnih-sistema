08fe6c67547e70d986ac84c24b6339c5be808823
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-28 21:56:25 +0100

    Zadatak 10 rjesenje - 11.09.2020. ispit

---
+++ 11-09-2020/OPOS -solution -exam_11-09-2020 -zad10.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Reducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            List<Tuple<string, string>> pairs = new List<Tuple<string, string>>();
+
+            while ((line = Console.ReadLine()) != "")
+            {
+                line = line.Trim();
+                Regex rgx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                string[] lineSplit = line.Split(' ');
+                foreach (string pieceOfLine in lineSplit)
+                {
+                    foreach (Match match in rgx.Matches(pieceOfLine))
+                    {
+                        string matchString = match.ToString();
+                        Console.WriteLine("{0}\t{1}", matchString.Split('@')[0], matchString.Split('@')[1]);
+                    }
+                }
+            }
+        }
+    }
+}
