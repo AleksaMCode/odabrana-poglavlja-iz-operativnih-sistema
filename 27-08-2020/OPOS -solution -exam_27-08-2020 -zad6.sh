e562008a97b4f3afa8db02dd7b05e6d534efcfbf
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-29 13:51:28 +0100

    Zadatak 6 update - 27.08.2020. ispit

--- 27-08-2020/OPOS -solution -exam_27-08-2020 -zad6.sh
+++ 27-08-2020/OPOS -solution -exam_27-08-2020 -zad6.sh
@@ -3,7 +3,7 @@ ls *.tap -1 > tap.list
 i=1 #counter
 while IFS= read -r tap_file
 do
-	Tapatap -i $tap_file 1>file$i.tap 2>&1
-	./file$i.tap > file$i.log
+	Tapatap -i $tap_file 1>file$i.bin 2>&1
+	./file$i.bin > file$i.log
 	let i++
 done < tap.list
