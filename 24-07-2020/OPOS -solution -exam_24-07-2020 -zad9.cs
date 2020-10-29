0cdcc9dcb9fa2171e172e0a73a23856b43b776a2
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-29 01:06:41 +0100

    Zadatak 9 update - 24.07.2020. ispit

--- 24-07-2020/OPOS -solution -exam_24-07-2020 -zad9.cs
+++ 24-07-2020/OPOS -solution -exam_24-07-2020 -zad9.cs
@@ -14,30 +14,26 @@ namespace Reducer
             List<Tuple<double, double>> newPairs = new List<Tuple<double, double>>();
 
 
-            while ((line = Console.ReadLine()) != null)
+            while ((line = Console.ReadLine()) != "")
             {
-                string[] twoValues = line.Trim().Split('\t');
+                string[] twoValues = line.Trim().Split(' ');
                 pairs.Add(new Tuple<double, double>(Convert.ToDouble(twoValues[0]), Convert.ToDouble(twoValues[1])));
             }
             pairs = pairs.OrderBy(pair => pair.Item1).ToList(); // ascending sort by key value
 
-            double max = pairs[pairs.Count - 1].Item1;
-            double n;
-
-            while (pairs.Count() != 0)
+            while (pairs.Count() > 0)
             {
-                n = pairs[0].Item1;
+                double n = pairs[0].Item1;
                 var keysInRange = pairs.Where(pair => pair.Item1 >= n && pair.Item1 < n + 1.0).ToList(); // finding keys in range [n,n+1)
 
                 if (keysInRange.Count > 1)
                 {
                     double averageValue = keysInRange.Sum(pair => pair.Item2) / keysInRange.Count;
 
-                    //pairs = pairs.Except(keysInRange).ToList();
                     foreach (var element in keysInRange)
                     {
-                        pairs.Remove(element);
                         newPairs.Add(new Tuple<double, double>(element.Item1, averageValue));
+                        pairs.Remove(element);
                     }
                 }
                 else
