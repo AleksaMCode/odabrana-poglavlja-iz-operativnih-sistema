803ab71a07540bcef5e7a85dba9314773e5f9641
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-29 15:10:15 +0100

    Zadatak 7 rjesenje - 24.07.2020. ispit

---
+++ 24-07-2020/OPOS -solution -exam_24-07-2020 -zad7.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<Task> tasks = new List<Task>();
+
+        //Rezultati funkcija - da ne bi dolazilo do duplih poziva F2 i F3 cuvamo rezulate u Dictionary
+        Dictionary<int, int> resF1 = new Dictionary<int, int>();
+        Dictionary<int, int> resF2 = new Dictionary<int, int>();
+
+        //Jasno je na osnovu teksta zadatka da se F1 mora izvrsiti za vrijednosti[1, 105] u sklopu poziva F2
+        for (int i = 1; i <= 105; ++i) {
+            if (tasks.Size() < 3) { //FaaS klaster omogucuje konkurentno izvrsavanje max. 3 funkcije
+                try {
+                    tasks.Add(ScheduleAsync(F1, i));
+                } catch (Exception e) {
+                    System.out.println("F1 - value " + i + "-> cluster busy");
+                }
+            } else {
+                for (Task task : tasks) {
+                    resF1.Add(i, Wait(task));
+                }
+                tasks.Clear();
+            }
+
+            //F2 izracunavanje na osnovu F1
+            if ((i >= 80 && i <= 105) && tasks.Size() < 3) {
+                try {
+                    tasks.Add(ScheduleAsync(F2, resF1[i]));
+                } catch (Exception e) {
+                    System.out.println("F1 - value " + i + "-> cluster busy");
+                }
+            } else {
+                for (Task task : tasks) {
+                    resF2.Add(i, Wait(task));
+                }
+                tasks.Clear();
+            }
+        }
+    }
+}
