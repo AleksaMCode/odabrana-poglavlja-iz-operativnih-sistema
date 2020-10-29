bb9e5212d23dc5b817d9552a0623284eef9c51a7
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-29 02:04:10 +0100

    Zadatak 9 rjesenje - 10.07.2020. ispit

---
+++ 10-07-2020/OPOS -solution -exam_10-07-2020 -zad9.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ssh root@241.22.12.1
+scp root@241.22.12.1:/output/map_reduce/out1.chunk
+hdfs dfs -mkdir /mydir
+hdfs dfs -copyFromLocal /output/map_reduce/out1.chunk /mydir
+chmod a+x runMapReduce.sh
+./runMapReduce.sh mapper.py reducer.py
+hdfs dfs -copyToLocal /mydir /output/map_reduce 
+# ili hdfs dfs -get /mydir/out1.chunk /output/map_reduce 
