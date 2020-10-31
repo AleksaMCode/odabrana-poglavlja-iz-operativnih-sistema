5f527257276ad6676cb930ee628f7c1ed04115af
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-31 15:56:14 +0100

    Zadatak 7 update - 24.07.2020. ispit

--- 24-07-2020/OPOS -solution -exam_24-07-2020 -zad7.java
+++ 24-07-2020/OPOS -solution -exam_24-07-2020 -zad7.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         List<Task> tasks = new List<Task>();
 
         //Rezultati funkcija
@@ -9,11 +9,7 @@ public class Main {
         //Jasno je na osnovu teksta zadatka da se F1 mora izvrsiti za vrijednosti[1, 105] u sklopu poziva F2
         for (int i = 1; i <= 105; ++i) {
             if (tasks.Size() < 3) { //FaaS klaster omogucuje konkurentno izvrsavanje max. 3 funkcije
-                try {
-                    tasks.Add(ScheduleAsync(F1, i));
-                } catch (Exception e) {
-                    System.out.println("F1 - value " + i + " -> cluster busy");
-                }
+                tasks.Add(ScheduleAsync(F1, i));
             } else {
                 for (Task task : tasks) {
                     resF1.Add(i, Wait(task));
@@ -24,11 +20,7 @@ public class Main {
             //F2 izracunavanje na osnovu F1
             if (i >= 80 && i <= 105) {
                 if (tasks.Size() < 3) {
-                    try {
-                        tasks.Add(ScheduleAsync(F2, resF1[i]));
-                    } catch (Exception e) {
-                        System.out.println("F2 - value " + i + " -> cluster busy");
-                    }
+                    tasks.Add(ScheduleAsync(F2, resF1[i]));
                 } else {
                     for (Task task : tasks) {
                         resF2.Add(i, Wait(task));
