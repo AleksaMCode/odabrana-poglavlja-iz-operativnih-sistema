32c278643eac21e945341dd33a7fb5292bb68cd3
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-31 15:54:21 +0100

    Zadatak 7 update - 30.10.2020. ispit

--- 30-10-2020/OPOS -solution -exam_30-10-2020 -zad7.java
+++ 30-10-2020/OPOS -solution -exam_30-10-2020 -zad7.java
@@ -6,7 +6,7 @@ public class Main {
         Dictionary<int, int> resF1 = new Dictionary<int, int>();
         Dictionary<int, int> resF2 = new Dictionary<int, int>();
 
-        //Jasno je na osnovu teksta zadatka da se f1 mora izvrsiti za vrijednosti[1, 105] u sklopu poziva f2
+        //Jasno je na osnovu teksta zadatka da se F1 mora izvrsiti za vrijednosti[1, 105] u sklopu poziva F2
         for (int i = 1; i <= 1084; ++i) {
             if (tasks.Size() < 4) { //FaaS klaster omogucuje konkurentno izvrsavanje max. 4 funkcije
                 tasks.Add(ScheduleAsync(F1, i));
@@ -17,7 +17,7 @@ public class Main {
                 tasks.Clear();
             }
 
-            //f2 izracunavanje na osnovu f1
+            //F2 izracunavanje na osnovu F1
             if (i >= 80 && i <= 105) {
                 if (tasks.Size() < 4) {
                     tasks.Add(ScheduleAsync(F2, resF1[i]));
