37b816a620fe1e221de9ab6cc692504fedc4f070
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-31 15:58:03 +0100

    Zadatak 7 update - 10.07.2020. ispit

--- 10-07-2020/OPOS -solution -exam_10-07-2020 -zad7.java
+++ 10-07-2020/OPOS -solution -exam_10-07-2020 -zad7.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         List<Task> tasks = new List<Task>();
 
         //Rezultati funkcija - da ne bi dolazilo do duplih poziva F2 i F3 cuvamo rezulate u Dictionary
@@ -9,11 +9,7 @@ public class Main {
         //Jasno je na osnovu teksta zadatka da se F1 mora izvrsiti za vrijednosti[1, 200] u sklopu poziva F2 i F3
         for (int i = 1; i <= 200; ++i) {
             if (tasks.Size() < 4) { //FaaS klaster omogucuje konkurentno izvrsavanje max. 4 funkcije
-                try {
-                    tasks.Add(ScheduleAsync(F1, i));
-                } catch (Exception e) {
-                    System.out.println("F1 - value " + i + " -> cluster busy");
-                }
+                tasks.Add(ScheduleAsync(F1, i));
             } else {
                 for (Task task : tasks) {
                     resF1.Add(i, Wait(task));
@@ -23,11 +19,7 @@ public class Main {
 
             //F2 izracunavanje na osnovu F1
             if (tasks.Size() < 4) {
-                try {
-                    tasks.Add(ScheduleAsync(F2, resF1[i]));
-                } catch (Exception e) {
-                    System.out.println("F2 - value " + i + " -> cluster busy");
-                }
+                tasks.Add(ScheduleAsync(F2, resF1[i]));
             } else {
                 for (Task task : tasks) {
                     resF2.Add(i, Wait(task));
@@ -37,11 +29,7 @@ public class Main {
 
             //F3 izracunavanje na osnovu F2
             if (tasks.Size() < 4) {
-                try {
-                    tasks.Add(ScheduleAsync(F3, resF3[i]));
-                } catch (Exception e) {
-                    System.out.println("F3 - value " + i + " -> cluster busy");
-                }
+                tasks.Add(ScheduleAsync(F3, resF3[i]));
             } else {
                 for (Task task : tasks) {
                     resF3.Add(i, Wait(task));
